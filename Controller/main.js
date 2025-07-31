e93184893e39d77319f1b2e2b6d67fcaa4d0a258
TzurielYeffet <Tzuriel66ye@yahoo.com>
2025-07-31 22:22:15 +0300

    add style to the page and added div to  the html structure for better design and style

--- Controller/main.js
+++ Controller/main.js
@@ -11,8 +11,67 @@ const userData = {
   },
   impsumText:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.",
-  friends: [],
+  friends: [{firstName: "Mason",
+    lastName: "Clark",
+    address: "Maple Street, Chicago",
+    photo: "https://randomuser.me/api/portraits/men/15.jpg",
+    quote: "Life is short, smile while you still have teeth.",
+    favoritePokemon: { name: "Charmander", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" },
+    impsumText: "Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent.",
+    friends: []
+  },
+  {
+    firstName: "Olivia",
+    lastName: "Martinez",
+    address: "Sunset Blvd, Los Angeles",
+    photo: "https://randomuser.me/api/portraits/women/65.jpg",
+    quote: "Do more things that make you forget to check your phone.",
+    favoritePokemon: { name: "Bulbasaur", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" },
+    impsumText: "Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.",
+    friends: []
+  },
+  {
+    firstName: "Ethan",
+    lastName: "Bennett",
+    address: "5th Avenue, New York",
+    photo: "https://randomuser.me/api/portraits/men/31.jpg",
+    quote: "Make mistakes. Learn. Grow. Repeat.",
+    favoritePokemon: { name: "Squirtle", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" },
+    impsumText: "Praesent mauris. Fusce nec tellus sed augue semper porta.",
+    friends: []
+  },
+  {
+    firstName: "Sophia",
+    lastName: "Nguyen",
+    address: "Broadway, Seattle",
+    photo: "https://randomuser.me/api/portraits/women/72.jpg",
+    quote: "Stay hungry. Stay foolish.",
+    favoritePokemon: { name: "Jigglypuff", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png" },
+    impsumText: "Mauris massa. Vestibulum lacinia arcu eget nulla.",
+    friends: []
+  },
+  {
+    firstName: "Lucas",
+    lastName: "Wright",
+    address: "Elm Street, Austin",
+    photo: "https://randomuser.me/api/portraits/men/45.jpg",
+    quote: "You miss 100% of the shots you don’t take.",
+    favoritePokemon: { name: "Meowth", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/52.png" },
+    impsumText: "Class aptent taciti sociosqu ad litora torquent per conubia nostra.",
+    friends: []
+  },
+  {
+    firstName: "Isabella",
+    lastName: "Hughes",
+    address: "Palm Grove Rd, Miami",
+    photo: "https://randomuser.me/api/portraits/women/53.jpg",
+    quote: "Creativity is intelligence having fun.",
+    favoritePokemon: { name: "Eevee", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png" },
+    impsumText: "In convallis. Nunc nec neque. Phasellus leo dolor, tempus non.",
+    friends: []
+  }],
 };
+
 const user = new User(userData);
 console.log(user);
 class Renderer {
@@ -20,17 +79,21 @@ class Renderer {
 
   renderPost(user) {
     $(
-      `.profile-info .profile-picture`).html(`<img src="${userData.photo}" alt="profile-picture">`);
-    $(`.profile-info .username`).text(userData.firstName);
-    $(`.profile-info .address`).text(userData.address);
-    $(`.middle-container .favorite-quote`).text(userData.quote);
-    $(`.favorite-pokemon .pokemon-name`).text(userData.favoritePokemon.name);
-    $(`.favorite-pokemon .pokemon-image`).html(`<img src=${userData.favoritePokemon.url}>`);
-    $(`.about-me .bacon-impsum`).text(userData.impsumText)
+      `.profile-container .profile-picture`).html(`<img src="${user.photo}" alt="profile-picture">`);
+    $(`.profile-info .username`).text(user.firstName);
+    $(`.profile-info .address`).text(user.address);
+    $(`.middle-container .favorite-quote .quote`).text(user.quote);
+    $(`.favorite-pokemon .pokemon-name`).text(user.favoritePokemon.name);
+    $(`.favorite-pokemon .pokemon-image`).html(`<img src=${user.favoritePokemon.url}>`);
+    $(`.about-me .bacon-impsum`).text(user.impsumText)
+
+    user.friends.forEach(friend => {
+      $(`.friends-section .friends-list`).append(`<li>${friend.firstName}</li>`)
+    })
 
 
 
   }
 }
 const render = new Renderer();
-render.renderPost(user);
+render.renderPost(userData);
